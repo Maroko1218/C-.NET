3bf11c3d201d6ffaea90c700d8752c48724cea22
Maroko1218 <markus.svhn@gmail.com>
2023-01-17 17:20:15 +0100

    Updated gitignore Task 1 in lab 1 complete

---
+++ Lab1/Task1.cs
@@ -0,0 +1,9 @@
+﻿namespace Task1 {
+    class Task1 {
+        static void Main(string[] args) {
+            for (int i = 0; i <= 30; i += 2) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
