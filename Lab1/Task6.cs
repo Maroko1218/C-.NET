9e7ca0b5f40ffb4d3aec9eed89bac64eb6b029a5
Maroko1218 <markus.svhn@gmail.com>
2023-01-17 18:14:15 +0100

    Task 6 complete!

---
+++ Lab1/Task6.cs
@@ -0,0 +1,7 @@
+namespace Task6 {
+    class Task6 {
+        static void task6(string[] args) {
+            Console.WriteLine(Convert.ToInt32(Console.ReadLine()) + Convert.ToInt32(Console.ReadLine()));
+        }
+    }
+}
